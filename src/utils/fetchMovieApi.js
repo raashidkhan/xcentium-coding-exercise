46f01fbab42a8d1634cde2149dd79cf9ecd73a23
Zainab <raashid.cbc@gmail.com>
2024-11-25 05:02:41 +0530

    base url updated with https

--- src/utils/fetchMovieApi.js
+++ src/utils/fetchMovieApi.js
@@ -1,12 +1,8 @@
 const API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
-
-console.log(API_KEY);
-
+const BASE_URL = "https://www.omdbapi.com";
 export const fetchMovies = async (searchTerm) => {
   const response = await fetch(
-    `http://www.omdbapi.com/?s=${encodeURIComponent(
-      searchTerm
-    )}&apikey=${API_KEY}`
+    `${BASE_URL}/?s=${encodeURIComponent(searchTerm)}&apikey=${API_KEY}`
   );
   const data = await response.json();
 
@@ -20,9 +16,7 @@ export const fetchMovies = async (searchTerm) => {
 export const fetchMovieDetails = async (id) => {
   console.log(id);
 
-  const response = await fetch(
-    `http://www.omdbapi.com/?i=${id}&apikey=${API_KEY}`
-  );
+  const response = await fetch(`${BASE_URL}/?i=${id}&apikey=${API_KEY}`);
   const data = await response.json();
 
   if (data.Response === "True") {
